46b7750d7fe54902e22c5adedde552b6b59b81db
EDWIN DIREN REGIS MAHUGNON NOUKPO <diren.noukpo@tekbot.io>
2026-05-16 07:56:33 +0100

    Add initial project structure with .gitignore, Docker files, and README documents

---
+++ make/docker.mk
@@ -0,0 +1,10 @@
+##
+## docker.mk for mecarosmaster [SSH: ROSMASTER-YAHBOOM] in /home/rosmaster/mecarosmaster/make
+##
+## Made by dirennoukpo
+## Login   <diren.noukpo@epitech.eu>
+##
+## Started on  Sat May 16 07:53:38 2026 dirennoukpo
+## Last update Sun May 16 07:53:40 2026 dirennoukpo
+##
+
